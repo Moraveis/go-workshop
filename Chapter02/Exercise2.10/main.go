31cff5e2b4a76f975eb5f2686b25f800980f0260
jvmoraveis <jvmoraveis@squaretrade.com>
2020-08-20 04:06:15 -0700

    Exercise 2.10: Looping Over a Map

---
+++ Chapter02/Exercise2.10/main.go
@@ -0,0 +1,18 @@
+// Exercise 2.10: Looping Over a Map
+
+package main
+
+import "fmt"
+
+func main() {
+
+	config := map[string]string{
+		"debug":    "1",
+		"logLevel": "warn",
+		"version":  "1.2.1",
+	}
+
+	for key, value := range config {
+		fmt.Println(key, "=", value)
+	}
+}
